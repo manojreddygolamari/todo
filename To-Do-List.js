6ef9b28fc7eba8ab99f0fedb40669ed3a3860241
manojreddygolamari <76006617+manojreddygolamari@users.noreply.github.com>
2021-08-16 17:34:51 +0530

    Add files via upload

---
+++ To-Do-List.js
@@ -0,0 +1,117 @@
+window.onload=function(){
+
+    var addListbtn=document.getElementById("quickadd");
+    var modalCancelbtn=document.getElementById("cancelcard");
+
+    var modalDiv=document.querySelector(".modal-container");
+
+    //modal visibility logic
+    addListbtn.addEventListener("click",function(){
+        modalDiv.style.visibility ="visible";
+        document.getElementById("availablecards").style.visibility="hidden";
+    });
+
+    modalCancelbtn.addEventListener("click",function(){
+        clearForm();
+        modalDiv.style.visibility ="hidden";
+        document.getElementById("availablecards").style.visibility="visible";
+    });
+
+    // clearing the form fields once card is added 
+    function clearForm()
+    {
+      var clear=document.querySelectorAll(".list-input");
+       for( var i in clear)
+       {
+           clear[i].value ='';
+       }
+    }
+
+   
+
+    //adding list
+    var listContainerarea=document.getElementById("listContainer");
+    var addCount=0;
+    var modalAddbtn=document.getElementById("addcard");
+    modalAddbtn.addEventListener("click",addList);
+    
+        function addList()
+        {
+                
+               var title=document.getElementById("card-title").value;
+               if(title!='')
+               {
+                var maincontainer=document.createElement("div");
+                maincontainer.classList.add('maincontainer');
+                listContainerarea.appendChild(maincontainer);
+
+                var subcontainer1=document.createElement("div");
+                subcontainer1.classList.add('subcontainer1');
+                maincontainer.appendChild(subcontainer1);
+
+                var list=document.createElement("div");
+                list.classList.add('list');
+                subcontainer1.appendChild(list);
+                
+                var listtitlediv=document.createElement("div");
+                listtitlediv.classList.add('listtitlediv');
+                list.appendChild(listtitlediv);
+                
+                var h3 = document.createElement("H3");
+                var t3 = document.createTextNode(title);
+                h3.appendChild(t3);
+                listtitlediv.appendChild(h3);
+ 
+                var h4 = document.createElement("H4");
+                var t4 = document.createTextNode("3");
+                h4.appendChild(t4);
+                listtitlediv.appendChild(h4);
+        
+                var trash=document.createElement("button");
+                trash.innerHTML='<i id="trash" class="far fa-trash-alt"></i>';
+                trash.classList.add('trash')
+                listtitlediv.appendChild(trash);
+
+                var subcontainer2=document.createElement("div");
+                subcontainer2.classList.add("subcontainer2");
+                maincontainer.appendChild(subcontainer2);
+
+                var Addcardinlist=document.createElement("button");
+                Addcardinlist.innerHTML="+ Add card";
+                Addcardinlist.classList.add("Addcardinlist");
+                subcontainer2.appendChild(Addcardinlist);
+                //card Modal
+                Addcardinlist.addEventListener('click',function(e)
+                {
+                    if(e)
+                    {
+                        const cardModalcontainer=document.querySelector(".cardmodalcontainer");
+                        const cancelCardmodal=document.getElementById("cancelcardmodal");
+                        cardModalcontainer.style.visibility ="visible";
+                        //modal visibility logic
+                    
+                        cancelCardmodal.addEventListener("click",function(){
+                            cardModalcontainer.style.visibility ="hidden";
+                            clearForm();
+                        }); 
+                     }
+                 });
+
+
+               }
+               else{
+                   alert("Title is required");
+               }
+               
+               clearForm();
+               modalDiv.style.visibility ="hidden";
+               addCount++
+               if(addCount!=0)
+               {
+                document.getElementById("availablecards").style.display="none";
+               }
+
+        }
+
+}
+
